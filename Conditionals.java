9a9c3e40a7df4657bfe5712a9b0cc03eabe6f9f3
Ayush kapoor <1999rkkapoor@gmail.com>
2026-05-03 21:16:38 +0530

    Conditionals

---
+++ Conditionals.java
@@ -0,0 +1,13 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else {
+            System.out.println("Grade: C or below");
+        }
+    }
+}
