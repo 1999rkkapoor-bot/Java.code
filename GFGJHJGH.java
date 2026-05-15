5d9dd0fc4345e9cfddd513b76109673a68d82a28
Ayush kapoor <1999rkkapoor@gmail.com>
2026-05-15 13:54:24 +0530

    GFGJHJGH

---
+++ GFGJHJGH.java
@@ -0,0 +1,22 @@
+
+
+public class GFGJHJGH {
+
+    
+    public static void main(String args[])
+    {
+
+        
+        Long varLong = 999999999999L;
+
+        
+        String str = varLong + " ";
+
+        
+        System.out.println("Converted type : "
+                           + str.getClass().getName());
+
+        
+        System.out.println(str);
+    }
+}
