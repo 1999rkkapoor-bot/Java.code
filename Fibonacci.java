112af13d65364b8b9b04b74e63d3f1e3ab1000ef
Ayush kapoor <1999rkkapoor@gmail.com>
2026-05-18 10:35:53 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int a = 0;
+        int b = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
