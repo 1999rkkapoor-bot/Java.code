2bcce995cf1e5c34b88eb2f0b160a7b8a2672464
Ayush kapoor <1999rkkapoor@gmail.com>
2026-05-18 10:34:34 +0530

    GuessGame

---
+++ GuessGame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess = 0;
+
+        System.out.println("Guess a number between 1 and 100");
+
+        while (guess != number) {
+
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("Too High!");
+            } else if (guess < number) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Correct! You guessed it.");
+            }
+        }
+
+        sc.close();
+    }
+}
